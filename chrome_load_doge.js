c0eb8854681bb4f8d8683a1eeb14123bcea39316
Lauri-Henrik Jalonen <ljalonen@cs.helsinki.fi>
2013-11-11 20:44:05 +0200

    so revamp, such codez

--- chrome_load_doge.js
+++ chrome_load_doge.js
@@ -1,7 +1,7 @@
 var loaded = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
   if (!loaded) {
-    chrome.tabs.executeScript(null, { file: "libdoge/libdoge.min.js" });
+    chrome.tabs.executeScript(null, { file: "libdoge.mod.min.js" });
     loaded = true;
   }
 });
